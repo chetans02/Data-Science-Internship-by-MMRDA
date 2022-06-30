16ca3e185f19bd905d1f5e67bd02d06a45362519
Chetan Sapkal <99116561+chetans02@users.noreply.github.com>
2022-06-30 21:04:51 +0530

    Add files via upload

---
+++ 11_Cooking Dataset.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble  import RandomForestClassifier
+
+rf=RandomForestClassifier(random_state=1)
+lr=LogisticRegression()
+nb=MultinomialNB()
+dt=DecisionTreeClassifier(random_state=0)
+
+d_c = ["moroccan","korean","japanese","vietnamese","brazilian","southern_us","british","cajun_creole","chinese","filipino","french","greek","indian","spanish","irish","russian","noroccan","italian","thai","mexican","jamaican"]
+df=pd.read_json("whats cooking/train.json")
+
+print(df["cuisine"].unique())
+x=df['ingredients']
+y=df['cuisine'].apply(d_c.index)
+
+df['all_ingredients']=df['ingredients'].map(";".join)
+cv=CountVectorizer()
+x=cv.fit_transform(df['all_ingredients'].values)
+
+X_train,X_test,y_train,y_test=train_test_split(x,y,random_state=0,test_size=0.3)
+
+rf.fit(X_train,y_train)
+r_pred=rf.predict(X_test)
+
+print("Random Forest:",accuracy_score(y_test,r_pred))
+
+
+"""
+OUTPUT:
+['greek' 'southern_us' 'filipino' 'indian' 'jamaican' 'spanish' 'italian'
+ 'mexican' 'chinese' 'british' 'thai' 'vietnamese' 'cajun_creole'
+ 'brazilian' 'french' 'japanese' 'irish' 'korean' 'moroccan' 'russian']
+Random Forest: 0.7578881206788184
+
+"""
